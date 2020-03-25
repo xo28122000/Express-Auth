8c4b353ec9087b5e864fc6253f923ab89ba681e3
xo28122000 <jainam.2000@yahoo.com>
2020-03-24 23:48:10 -0700

    mongo db init and connect

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
-var multer = require("multer");
-var upload = multer();
-var session = require("express-session");
+// var multer = require("multer");
+// var upload = multer();
+// var session = require("express-session");
 var cookieParser = require("cookie-parser");
 const crypto = require("crypto");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(upload.array());
 app.use(cookieParser());
+// app.use(upload.array());
 // app.use(session({ secret: "Your secret key" }));
+
+// mongo
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/test");
+let User = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+  })
+);
+
+// end
 
 const users = [
   {
@@ -21,6 +36,9 @@ const users = [
   }
 ];
 
+// This will hold the users and authToken related to users
+const authTokens = {};
+
 // get the hashed passcode
 const getHashedPassword = password => {
   const sha256 = crypto.createHash("sha256");
@@ -28,30 +46,73 @@ const getHashedPassword = password => {
   return hash;
 };
 
+const generateAuthToken = () => {
+  return crypto.randomBytes(30).toString("hex");
+};
+
 app.get("/", (req, res) => {
-  res.render("<HTML><body>hi!</body></HTML>");
+  res.send("home");
+});
+app.get("/protected", (req, res) => {
+  res.send("some protected data");
 });
 
 app.post("/signup", (req, res) => {
-  console.log(req.body);
-  const { email, name, password } = req.body;
-  if (email & name & password) {
-    if (users.find(user => user.email === email)) {
-      res.send("already registered");
+  let user = new User(req.body);
+  console.log(user);
+  user.save(error => {
+    if (error) {
+      if (error === 11000) {
+        console.log("email not unique");
+        res.send("use a unique email");
+      } else {
+        console.log("error in post: signup");
+        res.sendStatus(500);
+      }
     } else {
-      const hash = getHashedPassword(password);
-      users.push({
-        name,
-        email,
-        password: hash
-      });
-      res.send("Signed up");
+      // no error occured
+      res.send("secret data");
+    }
+  });
+  //   // // no mongo example
+  //     if (email && name && password) {
+  //       if (users.find(user => user.email === email)) {
+  //         res.send("already registered");
+  //       } else {
+  //         const hash = getHashedPassword(password);
+  //         users.push({
+  //           name,
+  //           email,
+  //           password: hash
+  //       });
+  //       res.send("Signed up");
+  //     }
+  //   } else {
+  //     console.log("data missing!");
+  //     res.sendStatus(400);
+  //   }
+});
+
+app.post("/signin", (req, res) => {
+  const { email, password } = req.body;
+  if (email && password) {
+    const hashedPassword = getHashedPassword(password);
+    const user = users.find(u => {
+      return u.email === email && hashedPassword === u.password;
+    });
+
+    if (user) {
+      const authToken = generateAuthToken();
+      authTokens[authToken] = user;
+      res.cookie("AuthToken", authToken);
+      res.send("some protected data");
+    } else {
+      res.send("not accessable");
     }
   } else {
     console.log("data missing!");
     res.sendStatus(400);
   }
-  console.log(users);
 });
 
 app.listen(5000, () => {
